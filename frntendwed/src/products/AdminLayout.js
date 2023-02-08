9f8988c90a423703f2eafc3c021dba46b10f7e93
Morning <morning@mstars-lab3-07s-iMac.local>
2023-02-08 11:58:41 +0800

    Almost done

--- frntendwed/src/products/AdminLayout.js
+++ frntendwed/src/products/AdminLayout.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AddProductMain = () => {
+  const productInit = {
+    productName: "",
+    price: 0,
+    InStock: 0,
+    categoryId: 0,
+    thumbImage: "",
+    images: [],
+  };
+  const [loading, setLoading] = useState(false);
+
+  const [productItem, setProductItem] = useState(productInit);
+  const navigate = useNavigate();
+
+  const onSave = (e) => {
+    // console.log("hahahah");
+    // e.preventDfefault();
+    axios.post("http://localhost:8080/api/product", productItem).then((res) => {
+      console.log(res);
+      if (res.data.status) {
+        navigate("/about");
+      } else {
+        alert(res.data.message);
+      }
+    });
+  };
+  const sendFile = (files) => {
+    console.log(files);
+    setLoading(true);
+    const formData = new FormData();
+    const url = "https://api.cloudinary.com/v1_1/djtvcia4j/upload";
+
+    formData.append("file", files[0]);
+    formData.append("api_key", "633168448776231");
+    formData.append("upload_preset", "zilnpuv0");
+
+    axios.post(url, formData).then((res) => {
+      setProductItem({ ...productItem, thumbImage: res.data.secure_url });
+      setLoading(false);
+      // console.log(res.data.secure_url);
+    });
+  };
   return (
     <>
       <section className="content-main" style={{ maxWidth: "1200px" }}>
@@ -12,7 +55,11 @@ const AddProductMain = () => {
             </Link>
             <h2 className="content-title">Add product</h2>
             <div>
-              <button type="submit" className="btn btn-primary">
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={onSave}
+              >
                 Publish now
               </button>
             </div>
@@ -32,6 +79,13 @@ const AddProductMain = () => {
                       className="form-control"
                       id="product_title"
                       required
+                      value={productItem.productName}
+                      onChange={(e) =>
+                        setProductItem({
+                          ...productItem,
+                          productName: e.target.value,
+                        })
+                      }
                     />
                   </div>
                   <div className="mb-4">
@@ -44,6 +98,13 @@ const AddProductMain = () => {
                       className="form-control"
                       id="product_price"
                       required
+                      value={productItem.price}
+                      onChange={(e) =>
+                        setProductItem({
+                          ...productItem,
+                          price: e.target.value,
+                        })
+                      }
                     />
                   </div>
                   <div className="mb-4">
@@ -56,6 +117,13 @@ const AddProductMain = () => {
                       className="form-control"
                       id="product_price"
                       required
+                      value={productItem.InStock}
+                      onChange={(e) =>
+                        setProductItem({
+                          ...productItem,
+                          InStock: e.target.value,
+                        })
+                      }
                     />
                   </div>
                   <div className="mb-4">
@@ -74,7 +142,14 @@ const AddProductMain = () => {
                       type="text"
                       placeholder="Inter Image URL"
                     />
-                    <input className="form-control mt-3" type="file" />
+                    <input
+                      className="form-control mt-3"
+                      type="file"
+                      onChange={(e) => {
+                        sendFile(e.target.files);
+                      }}
+                    />
+                    {loading && "Uploading..."}
                   </div>
                 </div>
               </div>
